00bc8c7cf83f9c81eb7d00fdcdda1177e4d13a43
suchshinde <ct32hin51s0>
2019-10-06 23:20:13 +0530

    Added Unit Test FrameWork

---
+++ tests/users.test.js
@@ -0,0 +1,27 @@
+/*const request=require('supertest');
+const app = require('../app');
+const jwt=require('jsonwebtoken');*/
+//const token='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiIyMzMzIiwicm9sZSI6ImFkbWluIiwiaWF0IjoxNTcwMzU5NTkxLCJleHAiOjE1NzAzNzc1OTF9.LHsoM6yAafwPGwKZKcz9EzfIT3GzAaljaVeIMF5Qk9c';
+
+/*
+test('Should Creat New User ',async ()=>{
+await request(app).post('/users').send({
+    firstname:'testUser',
+    lastname:'testLN',
+    email:'abc@gmail.com',
+    address:'Nashik',
+    mobile:'7588552943'
+}).expect(200);
+});
+*/
+
+
+/*
+test('Should Get All Users ', async () => {
+    await request(app)
+        .get('/users')
+        .set('Authorization', `Bearer ${token}`)
+        .send()
+        .expect(200);
+});
+*/
